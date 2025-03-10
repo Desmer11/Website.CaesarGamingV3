1d37eff32e7d2aec3335e6162f5c6137b17d6d28
Jonas SNK <148264630+Desmer11@users.noreply.github.com>
2025-03-10 21:16:42 +0100

    Merge branch 'master' of https://github.com/Desmer11/Website.CaesarGamingV3

---
+++ Website.CaesarGamingV3/wwwroot/lib/js/layout.js
@@ -0,0 +1,32 @@
+﻿const toggler = document.querySelector(".btn");
+toggler.addEventListener("click", function () {
+    document.querySelector("#sidebar").classList.toggle("collapsed");
+});
+
+// Collapse/Uncollapse Sidebar Items
+
+
+
+//window.addEventListener("resize", adjustBackground);
+
+//function adjustBackground() {
+//    const body = document.body;
+//    const viewportHeight = window.innerHeight;
+
+//    // Adjust background position dynamically
+//    body.style.backgroundPosition = `center calc(50% - ${(viewportHeight - body.offsetHeight) / 2}px)`;
+//}
+
+//// Trigger adjustment on page load
+//adjustBackground();
+
+
+
+
+
+
+
+
+
+
+
---
+++ Website.CaesarGamingV3/wwwroot/lib/js/music.js
@@ -0,0 +1,14 @@
+﻿// Select all audio players
+const audioPlayers = document.querySelectorAll('.audioPlayer');
+
+// Add event listeners to each audio player
+audioPlayers.forEach(player => {
+    player.addEventListener('play', () => {
+        // Pause all other players when a new one starts
+        audioPlayers.forEach(otherPlayer => {
+            if (otherPlayer !== player) {
+                otherPlayer.pause();
+            }
+        });
+    });
+});
